e0fc23935a19888d1c189d696cbed258585e9ed5
Juan Flores <jflores@findmypast.com>
2016-04-24 15:22:06 +0100

    Update HelloWorld.java

--- src/main/java/com/codurance/HelloWorld.java
+++ src/main/java/com/codurance/HelloWorld.java
@@ -3,6 +3,6 @@ package com.codurance;
 public class HelloWorld {
 
     public String sayHelloWorld() {
-        return "<img src='https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'/>"
+        return "<img src='https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'/>";
     }
 }
